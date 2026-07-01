2d4ab8a96851a4eb9e4667511271f870756ab6b3
isaakKareithi <isaacsgk2@gmail.com>
2026-07-01 18:07:22 +0300

    C1ClassConstructor.java

---
+++ C1ClassConstructor.java
@@ -0,0 +1,37 @@
+public class C1ClassConstructor{
+public static void main(String[] args) {
+    Student student = new Student("Alice", 20 "Computer Science");
+    System.out.println("Name: "+ student.getName());
+    System.out.println("Age: "+ student.getAge());
+    System.out.println("Major: "+ student.getMajor());
+    
+}
+public class Person{
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age; 
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+}
+
+public class Student extends Person{
+    private String major;
+
+    public Student(String name, int age, String major){
+    super(name, age);
+    this.major = major;
+}
+    public String getMajor(){
+        return major;
+    }
+}}
